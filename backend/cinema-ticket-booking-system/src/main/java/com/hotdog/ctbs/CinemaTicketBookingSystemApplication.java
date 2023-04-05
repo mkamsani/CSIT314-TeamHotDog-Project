7df69fbe3c5e59d2ff0eb006d6c1384c68a2d0ce
mkamsani <mkamsani@posteo.org>
2023-04-06 00:42:56 +0800

    Scaffold backend

---
+++ backend/cinema-ticket-booking-system/src/main/java/com/hotdog/ctbs/CinemaTicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hotdog.ctbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaTicketBookingSystemApplication.class, args);
+    }
+
+}
---
+++ backend/cinema-ticket-booking-system/src/test/java/com/hotdog/ctbs/CinemaTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotdog.ctbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
