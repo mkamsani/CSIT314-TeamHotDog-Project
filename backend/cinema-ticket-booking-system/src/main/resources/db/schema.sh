89cea636a80e73ff57ea151da45956eba7ecdcb0
mkamsani <mkamsani@posteo.org>
2023-04-19 21:33:11 +0800

    Update docker script
    The script will automatically kill any running instances of a postgres
    container and initialize a new one. In IntelliJ, right-click on the
    file schema.sh and select "Modify Run Configuration". In the "Run on:"
    option, select your WSL instance. This should be Ubuntu 20.04.
    
    The script will also create the necessary tables from schema.sql. This
    removes any need to manually run the sql file from the command line or
    IDE, upon first initialization.

--- backend/cinema-ticket-booking-system/src/main/resources/db/schema.sh
+++ backend/cinema-ticket-booking-system/src/main/resources/db/schema.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
-test "$(nerdctl -v | grep nerdctl)" && oci=nerdctl
-test "$(podman -v | grep podman)" && oci=podman
-test "$(docker -v | grep docker)" && oci=docker
-test -z "$oci" && echo "No OCI runtime found" && exit 1
-# Kill the container (to reset) if it's running.
+
+# Prerequisites:
+# sudo usermod -aG docker $(id -nu 1000) && exit
+# Run the above command, then run this script again.
+
+# -e : Terminate if a command exits with a non-zero status.
+# -u : Treat unset variables as an error when substituting.
+set -eu
+
+# Exit if script is not run from the source directory.
+test "$(basename "$(pwd)")" = "db" || exit 1
+# Exit if no OCI runtime is found.
+test "$(command -v nerdctl && nerdctl -v | grep nerdctl)" && oci=nerdctl
+test "$(command -v podman && podman -v | grep podman)" && oci=podman
+test "$(command -v docker && docker -v | grep docker)" && oci=docker
+test -z "$oci" && printf %s\\n "No OCI runtime found" && exit 1
+
+# Kill and remove the container (to reset) if it's running.
 "$oci" container list -a | grep pg && "$oci" rm -f pg
-# Start the postgres container in the background.
+# Start the container in the background.
 "$oci" run --rm --name="pg" -e POSTGRES_PASSWORD="pg" --net=host -d cgr.dev/chainguard/postgres:latest
-unset "$oci"
+# Allow time for postgres to start.
+sleep 1
+
+# Get absolute path of the schema.sql file
+schema_file="$(find "$(pwd)" -name "schema.sql" -type f -exec realpath {} \;)"
+# Copy the schema.sql file to the container
+"$oci" cp $schema_file pg:/
+
+# Enter the container and create the schema.
+"$oci" exec -it pg psql -U postgres -f /schema.sql
+
+# Unset variables from the environment.
+unset oci schema_file
