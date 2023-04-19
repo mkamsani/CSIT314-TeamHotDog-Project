7ed288aa43e8e35b4529026a2e9393dc9a475866
mkamsani <mkamsani@posteo.org>
2023-04-19 20:03:40 +0800

    Update docker script. Run to start/restart postgres

--- backend/cinema-ticket-booking-system/src/main/resources/db/schema.sh
+++ backend/cinema-ticket-booking-system/src/main/resources/db/schema.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-test "$(which -v podman)" && oci=podman
-test "$(which -v docker)" && oci=docker
+test "$(nerdctl -v | grep nerdctl)" && oci=nerdctl
+test "$(podman -v | grep podman)" && oci=podman
+test "$(docker -v | grep docker)" && oci=docker
 test -z "$oci" && echo "No OCI runtime found" && exit 1
+# Kill the container (to reset) if it's running.
+"$oci" container list -a | grep pg && "$oci" rm -f pg
 # Start the postgres container in the background.
-docker run --rm --name="pg" -e POSTGRES_PASSWORD="pg" --net=host -d cgr.dev/chainguard/postgres:latest
-# docker kill pg
+"$oci" run --rm --name="pg" -e POSTGRES_PASSWORD="pg" --net=host -d cgr.dev/chainguard/postgres:latest
+unset "$oci"
