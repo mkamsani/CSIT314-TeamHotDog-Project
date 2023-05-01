69826b504fb11fae98d5117573de3f3dca1a2bca
mkamsani <mkamsani@posteo.org>
2023-05-01 19:15:16 +0800

    Reverse proxy for frontend and backend

--- frontend/nginx.sh
+++ frontend/nginx.sh
@@ -1,6 +1,7 @@
 podman rm -f nginx
-#podman run --name nginx --net=host -v "$(pwd)"/nginx.conf:/etc/nginx/nginx.conf:rw -d docker.io/nginx:stable-alpine
+#podman run --rm --name nginx --net=host --security-opt label=disable  -d docker.io/nginx:stable-alpine
+podman run --rm --name nginx -e NGINX_PORT=8181 --security-opt label=disable -v "$(pwd)"/nginx.conf:/etc/nginx/nginx.conf:ro -dd docker.io/nginx:stable-alpine
 #podman run --name nginx --net=host --security-opt label=disable -d docker.io/nginx:stable-alpine
 #podman cp nginx:/etc/nginx/conf.d/default.conf .
 
-podman run --name nginx --net=host -v "$(pwd)"/default.conf:/etc/nginx/conf.d/default.conf:rw -d docker.io/nginx:stable-alpine
+#podman run --name nginx --net=host -v "$(pwd)"/default.conf:/etc/nginx/conf.d/default.conf:rw -d docker.io/nginx:stable-alpine
