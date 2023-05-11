b2df7b8c4db0fa8f165739a9c9fb4563b2dd2fa3
Justin <justinyiew@hotmail.com>
2023-05-11 15:53:22 +0800

    unit testing by Justin

---
+++ frontend/justin/Unit Testing/indexTest.php
@@ -0,0 +1,37 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class indexTest extends TestCase
+{
+    public function testLoginTrue()
+    {
+
+        // create our http client (Guzzle)
+        $client = new Client('http://localhost:8000', array(
+            'request.options' => array(
+                'exceptions' => false,
+            )
+        ));
+
+        $data = array(
+            'userId' => 'jim',
+            'password' => 'password-employee'
+
+        );
+
+        $request = $client->post('/api/user-account/login', null, json_encode($data));
+        $response = $request->send();
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertTrue($response->hasHeader('Location'));
+        $data = json_decode($response->getBody(true), true);
+        $this->assertArrayHasKey('nickname', $data);
+
+    }
+
+
+}
+
+?>
+
+?>
