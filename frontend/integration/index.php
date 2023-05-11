8b9aa5f89c44ee42f4240776ce23a36bf264a4cf
seanhoenjie <seanissofunny@gmail.com>
2023-05-11 13:52:46 +0800

    integration folder

---
+++ frontend/integration/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<?php
+// Initialize the session
+session_start();
+
+include('header.php');
+include('idx_nav.php');
+?>
+
+
+<body>
+
+<?php
+$result = '';
+if(isset($_POST['submit']) && !empty($_POST['userId']) && !empty($_POST['password']))
+{
+    $userId = $_POST['userId'];
+    $password = $_POST['password'];
+    $ch = curl_init();
+    $loginarr = array('userId' => $userId, 'password' => $password);
+
+    $json_data = json_encode($loginarr);
+
+    curl_setopt($ch, CURLOPT_URL, "http://localhost:8000/api/user-account/login");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json','Content-Length: ' . strlen($json_data)]);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $json_data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    $http_status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    // Check the user privilege and redirect
+    if ($http_status_code == 200)
+    {
+        // Set session variable.
+        $_SESSION["privilege"] = $result;
+    }
+}
+
+if(isset($_SESSION['privilege']))
+{
+    switch ($_SESSION["privilege"] )
+    {
+        case 'admin':
+            header("location: UserAdmin.php");
+            echo "<script>document.getElementById('result').innerHTML = '" . $_SESSION["privilege"] . "';</script>";
+            break;
+        case 'owner':
+//        header("location: owner/index.php"); TODO
+            echo "<script>document.getElementById('result').innerHTML = '" . $_SESSION["privilege"] . "';</script>";
+            break;
+        case 'manager': header("location: CinemaManager.php"); break;
+        case 'customer': header("location: Customer.php"); break;
+        default:
+            echo "<script>document.getElementById('result').innerHTML = '" . $_SESSION["privilege"] . "';</script>";
+            break;
+    }
+}
+
+?>
+
+<input type="text" name = "result" value = $result hidden>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style="margin-left: 20%; width: 60%">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="5000">
+            <img src="Pics\carousel-1.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h1 style ="color: red; text-shadow: 2px 2px #000">HotDog Cinemas</h1>
+            </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="5000">
+            <img src="Pics\carousel-2.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h1 style ="color: red; text-shadow: 2px 2px #000;">HotDog Cinemas</h1>
+            </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="5000">
+            <img src="Pics\carousel-3.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h1 style ="color: red; text-shadow: 2px 2px #000;">HotDog Cinemas</h1>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<div class="container mt-4">
+    <form class='form-wrapper mx-auto' action="<?php echo $_SERVER["PHP_SELF"]; ?>"
+          method='POST' name ="form1" style="width: 30%">
+        <div class="input-group mt-3">
+            <span class='input-group-text'>User ID : </span>
+            <input class='form-control' type='text' name='userId' required>
+        </div>
+        <div class="input-group mt-3">
+            <span class='input-group-text'>Password : </span>
+            <input class='form-control' type='password' name='password' required>
+        </div>
+        <span class="error" style="color:red" id = "result"><?php echo $result; ?></span>
+        <div class="mt-3 d-grid col-6 gap-2 mx-auto">
+            <input class="btn btn-danger" type="submit" name="submit" value="Log In">
+            <label>Don't have an account?</label>
+            <a class="btn btn-danger" href="register.php">Register</a>
+        </div>
+    </form>
+</div>
+</body>
