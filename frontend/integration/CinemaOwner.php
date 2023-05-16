8a1e5d1d16dfd12648741cc785741abb78cf2aaf
seanhoenjie <seanissofunny@gmail.com>
2023-05-16 20:13:19 +0800

    Added CinemaOwner page

---
+++ frontend/integration/CinemaOwner.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<?php
+session_start();
+include('header.php');
+
+$username = "";
+$privilege = "";
+
+if (isset($_SESSION["username"]))
+{
+    $username = $_SESSION["username"];
+    // Use the $username variable as needed
+}
+
+if (isset($_SESSION["privilege"]))
+{
+    $privilege = $_SESSION["privilege"];
+    // Use the $username variable as needed
+}
+
+// Retrieve the user data from the API
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://localhost:8000/api/admin/user-account/read/' . $username);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+$data = json_decode($result, true);
+$user = $data[0];
+curl_close($ch);
+
+
+?>
+
+
+<nav class="navbar navbar-expand-sm">
+    <div class="container">
+        <a class="navbar-brand">
+            <h1 style="width:25px; margin-bottom: 5px"> HOTDOG CINEMAS</h1>
+        </a>
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active bg-danger" href="CinemaOwner.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">User Accounts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">User Profiles</a>
+            </li>
+            &emsp;
+            <form method="get" class="d-flex">
+                <a class="btn btn-outline-danger" href="logout.php">Log Out</a>
+            </form>
+        </ul>
+    </div>
+</nav>
+
+<body>
+<div class="container-fluid p-5 bg-danger text-white text-center">
+    <h1>Welcome User Administrator!</h1>
+    <p>Username: <?php echo $username; ?></p>
+</div>
+
+<div class="container mt-6 row g-3 mx-auto" style="width: 20%">
+    <h1 class="text-center text-white">User Admin Information</h1>
+    <div class="col-md">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="name" value = "<?php echo $user['firstName']; ?>" readonly>
+            <label for="name">First Name: </label>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="surname" value = "<?php echo $user['lastName']; ?>" readonly>
+            <label for="surname">Last name: </label>
+        </div>
+    </div>
+    <div>
+        <div class="form-floating">
+            <input type="email" class="form-control" id="email" value = "<?php echo $user['email']; ?>" readonly>
+            <label for="email">E-Mail: </label>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="title" value = "<?php echo $user['title']; ?>" readonly>
+            <label for="phone">Title: </label>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="privilege" value = "<?php echo $privilege ?>" readonly>
+            <label for="phone">Privilege: </label>
+        </div>
+    </div>
+</div>
+
+<div class="text-center mt-3">
+    <a class="btn btn-danger" href="UserAccounts.php" role="button">View User Accounts</a>
+    <a class="btn btn-danger" href="UserProfiles.php" role="button">View User Profiles</a>
+    <a class="btn btn-danger" href="CreateUser.php" role="button">Create User Account</a>
+    <a class="btn btn-danger" href="CreateUserProfile.php" role="button">Create User Profile</a>
+</div>
+
+</body>
+<style>
+    .navbar .nav-link
+    {
+        color: white;
+    }
+
+    .navbar .nav-link:hover
+    {
+        transform: scale(1.1);
+    }
+
+    .navbar-brand
+    {
+        font-family: Cinzel, Arial, sans-serif;
+        font-size: 36px;
+        color: #e50914;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    }
+</style>
+<?php include('footer.php') ?>
+
+</html>
