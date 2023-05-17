f638117eda4bcb76aab7d68087321fffcc312675
Isaac Toh <isaacsimstudy@gmail.com>
2023-05-17 18:51:32 +0800

    ManagerCinemaRoomReadController temp undone

---
+++ backend/src/main/java/com/hotdog/ctbs/controller/manager/ManagerCinemaRoomReadController.java
@@ -0,0 +1,35 @@
+package com.hotdog.ctbs.controller.manager;
+
+import com.hotdog.ctbs.entity.CinemaRoom;
+import com.hotdog.ctbs.repository.CinemaRoomRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/manager/cinemaRoom")
+public class ManagerCinemaRoomReadController {
+
+    private final CinemaRoom cinemaRoom;
+
+    private final CinemaRoomRepository cinemaRoomRepository;
+
+    public ManagerCinemaRoomReadController(CinemaRoomRepository cinemaRoomRepository) {
+        this.cinemaRoomRepository = cinemaRoomRepository;
+        this.cinemaRoom = new CinemaRoom();
+    }
+
+    /**
+     * Read a JSON array of {@code CinemaRoom} object(s)
+     */
+
+    @GetMapping("/read/{param}")
+    public ResponseEntity<String> Read(@PathVariable final String param) {
+        try{
+            return ResponseEntity.ok(cinemaRoom.readCinemaRoom(cinemaRoomRepository, param));
+        }
+        catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
