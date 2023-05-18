927377469ce839b98742096cb8101f9ec9ab4315
MajikJoker <33824823+MajikJoker@users.noreply.github.com>
2023-05-18 17:26:22 +0800

    Customer pages

--- frontend/bhone/cBooking.php
+++ frontend/bhone/cBooking.php
@@ -2,22 +2,36 @@
 
 <?php
 
-$moviesDetailsCh = curl_init();
-  curl_setopt($moviesDetailsCh, CURLOPT_URL, "http://localhost:8000/api/customer/movie/read/active");
-  curl_setopt($moviesDetailsCh, CURLOPT_RETURNTRANSFER, 1);
-  $moviesDetails = curl_exec($moviesDetailsCh);
-  
+	$moviesDetailsCh = curl_init();
+	curl_setopt($moviesDetailsCh, CURLOPT_URL, "http://localhost:8000/api/customer/movie/read/all");
+	curl_setopt($moviesDetailsCh, CURLOPT_RETURNTRANSFER, 1);
+	$moviesDetails = curl_exec($moviesDetailsCh);
+	
+	curl_close($moviesDetailsCh);
+	$moviesDetails = json_decode($moviesDetails, true);
 
-  curl_close($moviesDetailsCh);
-  $moviesDetails = json_decode($moviesDetails, true);
-  //$movieName = array_column($moviesDetails, "title");
-
+	$columnTitle = array_column($moviesDetails, 'title');
+	$columnImg_Src = array_column($moviesDetails, 'imageUrl');
+	$combineArray = array_combine($columnTitle, $columnImg_Src);
 
 ?>
 <html>
 <head>
 	<title>Book Movie</title>
 	<link rel="stylesheet" href="cBooking.css">
+
+	<style>
+	#display .movie-image {
+		width: 200px; /* Adjust the width as desired */
+		height: auto; /* Maintain the aspect ratio */
+		display: block; /* Ensure the image is displayed as a block element */
+		margin-top: 20px;
+	}
+
+	#date {
+		margin-top: 20px;
+	}
+	</style>
 </head>
 <body>
 	<header>
@@ -30,99 +44,83 @@ $moviesDetailsCh = curl_init();
 		</ul>
 	</nav>
 	<main>
-		<?php
-			foreach ($moviesDetails as $movie) {
-				$title = $movie["title"];
-				$img_src = $movie['imageUrl'];
-			}
-		?>
-
 		<form action="loyalty.php" method="post">
 			<fieldset>
 				<legend>Choose a movie</legend>
 				
 				<label for="movie">Select a movie:</label>
 				<select name="movie" id="movie" required>
-					<option value="">--Select a movie--</option>
-					<?php
-						foreach ($moviesDetails as $movie) {
-							$title = $movie["title"];
-							$img_src = $movie['imageUrl'];
-
-							?>
-							<option value="<?php echo $title?>"><?php echo $title?></option>
-						<?php 
-						} 
-						?>
-					
-
+				<option value="">--Select a movie--</option>
+				<?php foreach ($moviesDetails as $movie) { ?>
+					<option value="<?php echo $movie['title']; ?>" data-img-src="<?php echo $movie['imageUrl']; ?>">
+					<?php echo $movie['title']; ?>
+					</option>
+				<?php } ?>
 				</select>
 
-				
-
 				<div id="display">
-					<!-- Images will display here -->
+				<!-- Images will display here -->
 				</div>
 
 				<script>
-					var movieDropdown = document.getElementById("movie");
-					var displayDiv = document.getElementById("display");
+				var movieDropdown = document.getElementById("movie");
+				var displayDiv = document.getElementById("display");
 
-					movieDropdown.addEventListener("change", function() {
-						var selectedOption = this.options[this.selectedIndex];
+				movieDropdown.addEventListener("change", function() {
+					var selectedOption = this.options[this.selectedIndex];
 
-						displayDiv.innerHTML = "";
-						
-						if (selectedOption.value !== "") {
-							var movieImage = document.createElement("img");
-							<?php 
-								$columnTitle = array_column($moviesDetails, 'title');
-								$columnImg_Src = array_column($moviesDetails, 'image_url');
-							
-								$combineArray = array_combine($columnTitle, $columnImg_Src);
-								foreach($combineArray as $columnTitle => $columnImg_Src) {
-									echo $columnTitle;
-								}
-								
-							?>
-							movieImage.src = ".com.com.com";
-							movieImage.classList.add("movie-image");
+					displayDiv.innerHTML = "";
 
-							var movieName = document.createElement("span");
-							movieName.textContent = selectedOption.text;
+					if (selectedOption.value !== "") {
+					var movieImage = document.createElement("img");
+					movieImage.src = selectedOption.getAttribute("data-img-src");
+					movieImage.classList.add("movie-image");
 
-							var movieOption = document.createElement("div");
-							movieOption.classList.add("movie-option");
-							movieOption.appendChild(movieImage);
-							movieOption.appendChild(movieName);
+					// var movieName = document.createElement("span");
+					// movieName.textContent = selectedOption.text;
 
-							displayDiv.appendChild(movieOption);
-						}
-					});
+					var movieOption = document.createElement("div");
+					movieOption.classList.add("movie-option");
+					movieOption.appendChild(movieImage);
+					//movieOption.appendChild(movieName);
+
+					displayDiv.appendChild(movieOption);
+					}
+				});
 				</script>
 				
 				<label for="date">Select a date:</label>
-				<input type="date" name="date" required>
-				
+				<input type="date" id="date" name="date" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('+1 month')); ?>" required>
+
 				<label for="time">Select a time:</label>
-				<select name="time" required>
+				<select name="time" required disabled>
 					<option value="">--Select a time--</option>
 					<option value="morning">Morning</option>
 					<option value="afternoon">Afternoon</option>
 					<option value="evening">Evening</option>
 				</select>
-				
+
 				<label for="ticketType">Type of Ticket:</label>
-				<select name="ticketType" required>
+				<select name="ticketType" required disabled>
 					<option value="">--Select a type--</option>
 					<option value="adult">Adult</option>
 					<option value="child">Child</option>
 					<option value="student">Student</option>
 					<option value="senior">Senior</option>
 				</select>
-				
-			</fieldset>
 
+				<script>
+					var dateInput = document.getElementById("date");
+					var timeSelect = document.getElementsByName("time")[0];
+					var ticketTypeSelect = document.getElementsByName("ticketType")[0];
+
+					dateInput.addEventListener("change", function() {
+						// Enable the time and ticket type selects when a date is selected
+						timeSelect.disabled = false;
+						ticketTypeSelect.disabled = false;
+					});
+				</script>
+			</fieldset>
 			<input type="submit" name="submit" value="Book Now">
 		</form>
 	</main>
