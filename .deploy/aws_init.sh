07b36d068a40007aefc998c2647460441f833016
Isaac Toh <isaacsimstudy@gmail.com>
2023-05-22 22:59:13 +0800

    aws_init service created properly

--- .deploy/aws_init.sh
+++ .deploy/aws_init.sh
@@ -29,15 +29,21 @@ mkdir -p ~/ctbs # Cinema Ticket Booking System
 
 # TODO Change /etc/nginx/sites-available/default to allow PHP files.
 
+# Completed: This will create a service "spring-boot-app" that will run the backend application.
+sudo tee /etc/systemd/system/spring-boot-app.service <<EOF
+
 [Unit]
 Description=My SpringBootApp Java REST Service
 [Service]
 User=ubuntu
+
 # The configuration file application.properties should be here:
 #change this to your workspace
 WorkingDirectory=/home/ubuntu/ctbs
-#path to executable.
+
+#Path to executable.
 #executable is a bash script which calls jar file
+#instead of going to a bash script it will now point straight to the jar file
 ExecStart=/usr/bin/java -jar backend-1.0.0-SNAPSHOT.jar
 
 SuccessExitStatus=143
@@ -46,9 +52,8 @@ Restart=on-failure
 RestartSec=5
 [Install]
 WantedBy=multi-user.target
-# TODO figure out what is server and config.yml
-#  server refers to the server class in the backend
-#  config.yml is the configuration file for the server
+
+EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable --now my-webapp.service
