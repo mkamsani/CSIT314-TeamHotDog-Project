2ec023335f8767ed5bc42c894e4c3e041f4101df
mkamsani <mkamsani@posteo.org>
2023-05-23 02:41:55 +0800

    Rename Spring Boot test file to restart backend

--- backend/src/test/java/com/hotdog/ctbs/UserStoryLoginTests.java
+++ backend/src/test/java/com/hotdog/ctbs/UserStoryLoginTests.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class CinemaTicketBookingSystemApplicationTests {
+class UserStoryLoginTests {
 
     @Autowired
     MockMvc mockMvc;
